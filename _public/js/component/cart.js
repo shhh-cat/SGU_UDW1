6084f26c553b9efa239a3078fbae9ab54b38c8a3
thien <main.laotienboi2804@gmail.com>
2020-12-12 20:36:43 +0700

    Fix bug - beta

--- _public/js/component/cart.js
+++ _public/js/component/cart.js
@@ -67,7 +67,7 @@ $("#countItem").prepend(cartItemCount);
 
 for (var i in dataCart) {
   
-  $('<div class="row mb-4"> <div class="col-md-5 col-lg-3 col-xl-3 d-flex align-items-center"> <div class="rounded mb-3 mb-md-0"> <img class="img-fluid d-block" src="'+dataCart[i].data.img[0]+'" alt="Sample"> </div> </div> <div class="col-md-7 col-lg-9 col-xl-9"> <div> <div class="d-flex justify-content-between"> <div> <h5>'+dataCart[i].data.name+'</h5> <p class="mb-3 text-muted text-uppercase small"></p> <p class="mb-2 text-muted text-uppercase small">Color: <label class="sm-square m-0" style="background-color:'+dataCart[i].color+';"></label></p> </div> <div> <div class="spinner-width"> <input class="input-spinner form-control-sm" product="'+i+'" type="number" value="'+dataCart[i].amount+'" min="1" max="100" step="1"/> </div> <small id="passwordHelpBlock" class="form-text text-muted text-center"></small> </div> </div> <div class="d-flex justify-content-between align-items-center"> <div> <a href="#" type="button" class="text-danger small text-uppercase mr-3"><i class="fas fa-trash-alt mr-1"></i> Remove item </a> <!-- <a href="#!" type="button" class="card-link-secondary small text-uppercase"><i class="fas fa-heart mr-1"></i> Move to wish list </a> --> </div> <p class="mb-0"><span><strong id="summary">$'+numberWithCommas(parseFloat(dataCart[i].data.price).toFixed(2))+'</strong></span></p class="mb-0"> </div> </div> </div> </div><hr class="mb-4">').insertBefore('#endCart');
+  $('<div class="row mb-4"> <div class="col-md-5 col-lg-3 col-xl-3 d-flex align-items-center"> <div class="rounded mb-3 mb-md-0"> <img class="img-fluid d-block" src="'+dataCart[i].data.img[0]+'" alt="Sample"> </div> </div> <div class="col-md-7 col-lg-9 col-xl-9"> <div> <div class="d-flex justify-content-between"> <div> <h5>'+dataCart[i].data.name+'</h5> <p class="mb-3 text-muted text-uppercase small"></p> <p class="mb-2 text-muted text-uppercase small">Color: <label class="sm-square m-0" style="background-color:'+dataCart[i].color+';"></label></p> </div> <div> <div class="spinner-width"> <input class="input-spinner form-control-sm" product="'+i+'" type="number" value="'+dataCart[i].amount+'" min="1" max="100" step="1"/> </div> <small id="passwordHelpBlock" class="form-text text-muted text-center"></small> </div> </div> <div class="d-flex justify-content-between align-items-center"> <div> <a href="#" onclick="doit()" data-confirm="Do you really want to remove this product?" type="button" class="text-danger small text-uppercase mr-3 remove"><i class="fas fa-trash-alt mr-1"></i> Remove item </a> <!-- <a href="#!" type="button" class="card-link-secondary small text-uppercase"><i class="fas fa-heart mr-1"></i> Move to wish list </a> --> </div> <p class="mb-0"><span><strong id="summary">$'+numberWithCommas(parseFloat(dataCart[i].data.price).toFixed(2))+'</strong></span></p class="mb-0"> </div> </div> </div> </div><hr class="mb-4">').insertBefore('#endCart');
 }
 
 var d = new Date();
@@ -85,3 +85,17 @@ $( "input.input-spinner" ).change(function() {
 
 
 $('#timeShip').prepend(a.toDateString() + " - " + b.toDateString());
+var jbox = new jBox('Confirm', {
+  confirmButton: 'Yes, I sure',
+  cancelButton: 'Nope'
+});
+
+function doit(argument) {
+  
+  var myModal = new jBox('Modal', {
+              content: 'The website is buggy, please buy them to help us fix the error!!',
+              addClass: 'bg-danger'
+            });
+             
+            myModal.open();
+}
